815a8c7bec40310a87b0640948d4460ee4f33ec9
alfian <alfianwilfredohoris@gmail.com>
2023-01-27 00:13:39 +0700

    slicing

--- client-customer/src/views/Home.jsx
+++ client-customer/src/views/Home.jsx
@@ -19,58 +19,62 @@ export default function Home() {
 
   let changeHandler = (e) => {
     setS(e.target.value);
-    console.log(e.target.value);
   };
 
   return (
     <div className="grid grid-cols-12">
-      <div className="col-start-2 col-end-12 mt-[12px] ">
-        <div className="flex flex-row justify-between">
-          <div className="font-bold grid content-center">
-            <div>
-              <p>Welcome, Irene</p>
+      <div className="col-start-2 col-end-12 flex flex-col gap-y-[40px]">
+        <div className=" mt-[12px] ">
+          <div className="flex flex-row justify-between">
+            <div className="font-bold grid content-center">
+              <div>
+                <p>Welcome, Irene</p>
+              </div>
+            </div>
+            <div className="flex flex-row relative">
+              <div>
+                <img src={prof} alt="" srcset="" onClick={change} />
+              </div>
+              {statusDD ? (
+                <div className="fixed top-[60px] right-[10px] bg-[#B56A6A] w-[70px] rounded flex justify-center">
+                  <Dropdown />
+                </div>
+              ) : null}
             </div>
           </div>
-          <div className="flex flex-row relative">
+          <div>
             <div>
-              <img src={prof} alt="" srcset="" onClick={change} />
+              <p>Delivery to</p>
             </div>
-            {statusDD ? (
-              <div className="fixed top-[60px] right-[10px] bg-[#B56A6A] w-[70px] rounded flex justify-center">
-                <Dropdown />
+            <div className="flex flex-row">
+              <div>
+                <img src={mapMarker} alt="" className="w-[20px]" />
               </div>
-            ) : null}
+              <div className="text-[13px]">
+                <p>Universitas advent indonesia (Ester Extension No.1)</p>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-12 mt-[20px]">
+            <div className="col-span-12 flex flex-row relative grid grid-cols-12">
+              <div className="col-span-12">
+                <input
+                  type="text"
+                  onChange={changeHandler}
+                  placeholder="What would you like to order ?"
+                  className="outline outline-offset-[7px] w-full outline-[2px] placeholder:italic placeholder:text-black/50 h-[30px] text-[14px] outline-[#B56A6A] rounded-[2px] px-[5px] pr-[50px]"
+                />
+              </div>
+              <div className="fixed right-[50px]">
+                <button onClick={cari}>
+                  <img src={searchIcon} alt="" className="w-[30px]" />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
         <div>
-          <div>
-            <p>Delivery to</p>
-          </div>
-          <div className="flex flex-row">
-            <div>
-              <img src={mapMarker} alt="" className="w-[20px]" />
-            </div>
-            <div className="text-[13px]">
-              <p>Universitas advent indonesia (Ester Extension No.1)</p>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-12 mt-[20px]">
-          <div className="col-span-12 flex flex-row relative grid grid-cols-12">
-            <div className="col-span-12">
-              <input
-                type="text"
-                onChange={changeHandler}
-                placeholder="What would you like to order ?"
-                className="outline outline-offset-[7px] w-full outline-[2px] placeholder:italic placeholder:text-black/50 h-[30px] text-[14px] outline-[#B56A6A] rounded-[2px] px-[5px] pr-[50px]"
-              />
-            </div>
-            <div className="fixed right-[50px]">
-              <button onClick={cari}>
-                <img src={searchIcon} alt="" className="w-[30px]" />
-              </button>
-            </div>
-          </div>
+          <div className="font-bold">Recomended !</div>
         </div>
       </div>
     </div>
