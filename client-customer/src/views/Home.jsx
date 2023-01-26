eb5b388216b53ea42460800c2a7e9c50691e196c
alfian <alfianwilfredohoris@gmail.com>
2023-01-26 22:46:19 +0700

    working on dropdown

--- client-customer/src/views/Home.jsx
+++ client-customer/src/views/Home.jsx
@@ -1,13 +1,26 @@
+import prof from "../assets/prof.svg";
+import Dropdown from "../components/Dropdown";
+import down from "../assets/down.svg";
+import { useState } from "react";
 export default function Home() {
+  const [statusDD, setStatusDD] = useState(false);
   return (
     <div className="grid grid-cols-12">
       <div className="col-start-2 col-end-12 mt-[12px]">
         <div className="flex flex-row justify-between">
-          <div>
-            <p>Welcome, Irene</p>
+          <div className="font-bold grid content-center">
+            <div>
+              <p>Welcome, Irene</p>
+            </div>
           </div>
-          <div>
-            <button>Logout</button>
+          <div className="flex flex-row">
+            <div>
+              <img src={prof} alt="" srcset="" />
+            </div>
+            <div>
+              <img src={down} alt="" srcset="" />
+            </div>
+            {statusDD ? <Dropdown /> : null}
           </div>
         </div>
         <div>
