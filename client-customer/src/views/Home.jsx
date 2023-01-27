5ec5b94f097358e9ecf4409ec30514394b6d45b7
alfian <alfianwilfredohoris@gmail.com>
2023-01-27 10:54:16 +0700

    slicing card

--- client-customer/src/views/Home.jsx
+++ client-customer/src/views/Home.jsx
@@ -1,9 +1,8 @@
-import enak from "../assets/enak.jpeg";
 import Hello from "../components/Hello";
-import { useState } from "react";
 import ProfileButton from "../components/ProfileButton";
 import Address from "../components/Address";
 import Search from "../components/Search";
+import CardHome from "../components/CardHome";
 export default function Home() {
   return (
     <div className="grid grid-cols-12 font-jakarta">
@@ -32,20 +31,7 @@ export default function Home() {
         <div className="flex flex-col gap-y-[10px]">
           <div className="font-bold text-[19px]">Recommended !</div>
           <div>
-            <div className="bg-[#FFE5E5] rounded-[40px] shadow-lg drop-shadow-lg w-[48%] grid grid-cols-12">
-              <div className="col-start-2 col-end-12 py-[20px]">
-                <div className=" flex justify-center">
-                  <div>
-                    <img src={enak} alt="" className="w-[60px] rounded-full" />
-                  </div>
-                </div>
-                <div className="font-bold text-[18px]">Tirex</div>
-                <div className="text-[9px] font-semibold">
-                  Ayam goreng + nasi + sambal + lalapan
-                </div>
-                <div className="font-semibold">Rp. 15.000</div>
-              </div>
-            </div>
+            <CardHome />
           </div>
         </div>
       </div>
