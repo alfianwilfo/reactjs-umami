1991076acc0acbbad6acebbee84e1be50bc40dce
alfian <alfianwilfredohoris@gmail.com>
2023-01-27 11:12:16 +0700

    init reusable component

--- client-customer/src/views/OrderStatus.jsx
+++ client-customer/src/views/OrderStatus.jsx
@@ -1,8 +1,14 @@
-import Hello from "../components/Hello";
+import WelcomeDeliverProfile from "../components/WelcomeDeliverProfile";
 export default function OrderStatus() {
   return (
     <>
-      <Hello />
+      <div className="grid grid-cols-12 font-jakarta">
+        <div className="col-start-2 col-end-12 flex flex-col gap-y-[40px]">
+          <div>
+            <WelcomeDeliverProfile />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
