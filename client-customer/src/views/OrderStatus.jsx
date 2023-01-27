93b17896895dde1ea9a5b60b03ee3b96b5dca870
alfian <alfianwilfredohoris@gmail.com>
2023-01-27 12:05:34 +0700

    add note

--- client-customer/src/views/OrderStatus.jsx
+++ client-customer/src/views/OrderStatus.jsx
@@ -12,10 +12,14 @@ export default function OrderStatus() {
       </div>
       <div className="w-screen mt-[10px]">
         <div className=" h-[100px] mx-[10px] p-[20px] shadow-md drop-shadow-md ">
-          <div className="flex justify-between">
-            <div>2 x Tirex</div>
-            <div>Rp. 20.000</div>
+          <div className="flex flex-col">
+            <div className="flex justify-between">
+              <div>2 x Tirex</div>
+              <div>Rp. 20.000</div>
+            </div>
+            <div className="text-[12px]">note: Mau extra sambel</div>
           </div>
+          <div></div>
         </div>
       </div>
     </>
