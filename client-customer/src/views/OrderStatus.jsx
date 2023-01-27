83bc5c7518e1e02fd34dcd9582f5ffd9250c5457
alfian <alfianwilfredohoris@gmail.com>
2023-01-27 12:48:39 +0700

    add timeline card

--- client-customer/src/views/OrderStatus.jsx
+++ client-customer/src/views/OrderStatus.jsx
@@ -1,4 +1,8 @@
 import WelcomeDeliverProfile from "../components/WelcomeDeliverProfile";
+import done from "../assets/ceklisdone.svg";
+import undone from "../assets/ceklisundone.svg";
+import linedone from "../assets/linedone.svg";
+import lineundone from "../assets/lineundone.svg";
 export default function OrderStatus() {
   return (
     <>
@@ -11,7 +15,7 @@ export default function OrderStatus() {
         </div>
       </div>
       <div className="w-screen mt-[10px]">
-        <div className=" h-[100px] mx-[10px] p-[20px] shadow-md drop-shadow-md ">
+        <div className="mx-[10px] p-[20px] shadow-md drop-shadow-md flex flex-col gap-y-[10px] ">
           <div className="flex flex-col">
             <div className="flex justify-between">
               <div>2 x Tirex</div>
@@ -19,7 +23,45 @@ export default function OrderStatus() {
             </div>
             <div className="text-[12px]">note: Mau extra sambel</div>
           </div>
-          <div></div>
+          <div className="flex justify-center flex-row gap-x-[5px]">
+            <div className="grid content-center ">
+              <div className="flex justify-center">
+                <div>
+                  <img src={done} alt="" className="w-[22px]" />
+                </div>
+              </div>
+            </div>
+            <div className="grid content-center">
+              <div className="flex justify-center">
+                <img src={linedone} alt="" />
+              </div>
+            </div>
+            <div className="grid content-center">
+              <div>
+                <img src={undone} alt="" className="w-[22px]" />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img src={lineundone} alt="" />
+              </div>
+            </div>
+            <div className="grid content-center">
+              <div>
+                <img src={undone} alt="" className="w-[22px]" />
+              </div>
+            </div>
+            <div>
+              <div>
+                <img src={lineundone} alt="" />
+              </div>
+            </div>
+            <div className="grid content-center">
+              <div>
+                <img src={undone} alt="" className="w-[22px]" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
